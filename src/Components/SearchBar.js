7ed43f00cdd84068128d721ac0c64d0eff32cf68
Richa176 <rich.bhasin@gmail.com>
2022-07-09 16:28:27 +0530

    update

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+import SearchIcon from "@material-ui/icons/Search";
+import CloseIcon from "@material-ui/icons/Close";
+
+function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const current = new Date();
+  const date = `${current.getDate()} ${monthNames[current.getMonth()]} ${current.getFullYear()}`;
+
+  // return (
+  //   <div className="App">
+  //     <h1>Current date is {date}</h1>
+  //   </div>
+  // );
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+  return (
+    <div className="search">
+     <div className="header">
+             <h1>Hi, Richa Saidha!</h1>
+             <p>Here are your stats for today {date}</p>
+    </div>
+    <div className="header2">
+             <h3>Stay Healthy</h3>
+             <h4>Explore your full library of Premium resources for sleep, activity, mindfullness and nutrition</h4>
+             
+             <div className="btn_con">
+              <button className="btn">
+              Explore Premium
+              </button>
+
+              {/* <div className="">
+               </div> */}
+             </div>
+                
+    </div>
+  
+       <h1>Hi, Richa Saidha!</h1>
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+        <div className="searchIcon">
+          {filteredData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" onClick={clearInput} />
+          )}
+        </div>
+      </div>
+      {filteredData.length != 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <a className="dataItem" href={value.link} target="_blank">
+                <p>{value.title} </p>
+              </a>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SearchBar;
